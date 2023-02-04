449d6b178e7f33ff1ea5682900da902f9ac8cdb6
rci1337 <109876671+rci1337@users.noreply.github.com>
2023-02-04 15:36:03 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log("Developed by Aori#0001 // discord.gg/fan All rights reserved.");
+
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.4;
+  audio.play()
+}
+
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.play()
+}
---
+++ assets/js/typed.js
@@ -0,0 +1,9 @@
+new Typed('#typed',{
+    strings : [,'Day 1s like, truth, .gg/bullets .gg/fan','Bad at Lua, Artist Graphic',],
+    typeSpeed : 20,
+    delaySpeed : 1,
+    showCursor: false,
+    loop : true
+})
+
+
